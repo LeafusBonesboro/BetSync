114a05d0f923221bd969e0c373c15feef8fceafd
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2025-11-06 17:51:33 -0500

    fixed vercel dep

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import LoadBets from "../components/LoadBets";
+import BetsList from "../components/BetsList";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white gap-10 p-8">
       <h1 className="text-4xl font-bold text-blue-400">BetSync</h1>
      
-      <LoadBets />
+      <BetsList/>
     </main>
   );
 }
