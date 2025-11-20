1580760bdd06044ee08b69231bb7ad6b603acf14
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2025-11-19 19:18:50 -0500

    added next.config.ts logic for discord auth pathing

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/auth/:path*",
+        destination: "https://betsync-backend.onrender.com/auth/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
